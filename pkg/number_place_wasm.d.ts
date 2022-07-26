dd647b0e370eed596fa464c7812ee74164984a0c
gw31415 <gw31415@users.noreply.github.com>
2022-07-26 07:18:37 +0000

    deploy: 7ccec63ea5953bc908f0e16e34977e70974d6ced

---
+++ pkg/number_place_wasm.d.ts
@@ -0,0 +1,115 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* Seekerによる探索の現在の状態を表します。
+*/
+export enum SeekerState {
+/**
+* Seekerによる探索が終了したことを示します。
+*/
+  Finished,
+/**
+* 1つの仮定が失敗したことを示します。
+*/
+  TryFail,
+/**
+* 1つの仮定が失敗しなかったことを示します。
+*/
+  TryContinue,
+/**
+* 1つの解答が発見されたことを示します。
+*/
+  Found,
+}
+/**
+* この構造体にナンプレの数字を書き込んでいきます。
+*/
+export class Field {
+  free(): void;
+/**
+*/
+  constructor();
+/**
+* ナンプレに数字を記入します。
+* @param {number} i
+* @param {number} value
+*/
+  insert(i: number, value: number): void;
+/**
+* 指定した位置に入る可能性のある値一覧を返します。
+* @param {number} i
+* @returns {Uint32Array}
+*/
+  possiblity_at(i: number): Uint32Array;
+}
+/**
+* Seekerの探索の現在の進捗を表す構造体です。
+*/
+export class Report {
+  free(): void;
+/**
+* 進捗メッセージを文字列で表します。
+*/
+  readonly msg: string;
+/**
+* 解答が見つかった場合、解答を返します。
+*/
+  readonly result: Field | undefined;
+/**
+* 進捗をEnumで返します。
+*/
+  readonly state: number;
+}
+/**
+* ナンプレの解答を探索する構造体です。
+*/
+export class Seeker {
+  free(): void;
+/**
+* @param {Field} field
+*/
+  constructor(field: Field);
+/**
+* 探索を1ステップ進め、Reportを返します。
+* @returns {Report}
+*/
+  next(): Report;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_field_free: (a: number) => void;
+  readonly field_new: () => number;
+  readonly field_insert: (a: number, b: number, c: number, d: number) => void;
+  readonly field_possiblity_at: (a: number, b: number, c: number) => void;
+  readonly __wbg_seeker_free: (a: number) => void;
+  readonly seeker_new: (a: number) => number;
+  readonly seeker_next: (a: number) => number;
+  readonly __wbg_report_free: (a: number) => void;
+  readonly report_msg: (a: number, b: number) => void;
+  readonly report_state: (a: number) => number;
+  readonly report_result: (a: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* Synchronously compiles the given `bytes` and instantiates the WebAssembly module.
+*
+* @param {BufferSource} bytes
+*
+* @returns {InitOutput}
+*/
+export function initSync(bytes: BufferSource): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/number_place_wasm_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_field_free(a: number): void;
+export function field_new(): number;
+export function field_insert(a: number, b: number, c: number, d: number): void;
+export function field_possiblity_at(a: number, b: number, c: number): void;
+export function __wbg_seeker_free(a: number): void;
+export function seeker_new(a: number): number;
+export function seeker_next(a: number): number;
+export function __wbg_report_free(a: number): void;
+export function report_msg(a: number, b: number): void;
+export function report_state(a: number): number;
+export function report_result(a: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;

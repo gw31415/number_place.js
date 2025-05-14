88826677d65fd6f120445afcdac375b76f44290a
gw31415 <gw31415@amas.dev>
2025-05-14 15:29:58 +0900

    refactor: unused type

--- hono/app/server.ts
+++ hono/app/server.ts
@@ -1,10 +1,10 @@
-import { type Env, Hono } from "hono";
+import { Hono } from "hono";
 import { csrf } from "hono/csrf";
 import { showRoutes } from "hono/dev";
 import { secureHeaders } from "hono/secure-headers";
 import { createApp } from "honox/server";
 
-const base = new Hono<Env>();
+const base = new Hono();
 base.use(csrf());
 base.use(secureHeaders());
 
